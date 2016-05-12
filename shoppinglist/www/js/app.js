9e14ec8e327f9cc57cfe7153e587fc49dceee1e5
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-05-12 21:59:32 +0300

    added some functionality and styles

--- shoppinglist/www/js/app.js
+++ shoppinglist/www/js/app.js
@@ -9,10 +9,31 @@ angular.module("starter", ["ionic", "firebase"])
   $scope.items = Items;
   $scope.addItem = function() {
     var name = prompt("What do you need to buy?");
+    var needed = false;
     if (name) {
       $scope.items.$add({
-        "name": name
+        "name": name,
+        "needed": needed
       });
     }
   };
+
+  $scope.changeStatus = function(){
+     if(this.item.needed == false){
+        this.item.needed = true;
+        $scope.items.$save(this.item);
+     }else{
+        this.item.needed = false;
+        $scope.items.$save(this.item);
+     }
+  }
+  
+    $scope.deleteItem = function(){
+      var r = confirm("Delete " + this.item.name + " from the shopping list?");
+      if(r == true){
+         $scope.items.$remove(this.item);
+      }else{
+        return;
+      }
+  }
 });
